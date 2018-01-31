c140ff5d62cce0b6a2b7ee6b93a850ac004fbc29
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-01-31 15:44:26 +0530

    prime.C
    prime or not

--- prime.C
+++ prime.C
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int n,i;
+int n,i,remainder;
 printf("enter the number:");
 scanf("%d",&n);
+  for(i=2;i<=n;i++)
+  {
+    remainder=n%i;
+   
+  if(remainder!=0)
+  
+    printf("the number is prime");
+    else
+printf("the number is not prime");
+}
+return 0;
